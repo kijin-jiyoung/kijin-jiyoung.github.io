5801c2f7d49aaf9ecffb10943018ee71515a744f
미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>
2023-04-24 17:52:36 +0900

    Update index.js
    Signed-off-by: 미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function loadComments(auth) {
         <li>
           <p style="font size="16px;"><b>${comments[i].title}</b>
             <small>${moment(comments[i].created_at
-                  .toDate()
-                  .toLocaleString()}
-                  </small>
+               .replace("T", "  ")
+              .replace("Z", "")
+              .slice(0, -3)}</small>
           </p>
           <p style="text-indent: 10px;font-size:14px;">${comments[i].body}</p>
         </li><hr>`;
@@ -68,7 +68,7 @@ let $time = document.getElementById("time");
 let $date = document.getElementById("date");
 
 function getTime() {
-  const now = new Date('2023-03-09 00:00:00');
+  const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth() + 1;
   const date = now.getDate();
