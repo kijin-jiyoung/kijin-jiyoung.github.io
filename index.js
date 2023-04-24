6771e2ffcbd85bd4b161c6dfaefd9ca68a547ab3
미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>
2023-04-24 14:45:26 +0900

    Update index.js
    Signed-off-by: 미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -64,15 +64,16 @@ function registerComment(auth) {
         $nickname.value = "";
         $commentInput.value = "";
         window.location.reload();
-      });
-    }
-  });
-}
-
-function getTime() {
+        function getTime() {
 const now = new Date(); // 현재 시간
 const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000); // 현재 시간을 utc로 변환한 밀리세컨드값
 const koreaTimeDiff = 9 * 60 * 60 * 1000; // 한국 시간은 UTC보다 9시간 빠름(9시간의 밀리세컨드 표현)
 const koreaNow = new Date(utcNow + koreaTimeDiff); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
 $date.innerText = year + month + date + amOrPm + " " + hour + ":" + minute + ":" + second;
 }
+      });
+    }
+  });
+}
+
+
