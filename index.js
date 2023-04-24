e999e26f961be787e6945e3e30b61b5bb296dffe
미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>
2023-04-24 13:38:08 +0900

    Update index.js
    Signed-off-by: 미녀의 우당탕탕 개발기 <68098551+yooonjy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function loadComments(auth) {
       for (let i in comments) {
          $commentList.innerHTML += `
         <li>
-          <p style><b>${comments[i].title}<font size="18px;"></b>  
+          <p style><font size="16px;"><b>${comments[i].title}</b>
             <small>${comments[i].created_at
               .replace("T", "  ")
               .replace("Z", "")
